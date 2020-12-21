89965be3ba35d48c75f2093ccf79d21cc08b4453
Bima Arya Saputra Wibowo <62773300+Aeriya2@users.noreply.github.com>
2020-12-21 22:05:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_116_BimaAryaSaputraWibowo;
+using System.ServiceModel;
+
+namespace ServerConfigRest_116_BimaAryaSaputraWibowo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
